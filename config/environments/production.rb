23f69cb631eaff67a7c4b279529db50ab431d7bc
Rene Polo <renejpolo@me.com>
2013-04-01 10:49:44 -0400

    corregido

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ config.action_mailer.smtp_settings = {
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-    config.action_mailer.default_url_options = { :host => '198.211.110.12' }
+    #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
